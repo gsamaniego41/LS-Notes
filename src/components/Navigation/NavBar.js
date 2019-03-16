079828d5a7702505ba281bd5a11e2261f2ae939d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-16 11:18:35 -0700

    basic material navbar

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,11 +1,24 @@
 import React from "react";
 import {withStyles} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/core/Menu";
 
 const NavBar = props => {
   return (
     <div>
-      <AppBar position="fixed">Nav</AppBar>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography>Lambda Notes</Typography>
+          <Button>Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
