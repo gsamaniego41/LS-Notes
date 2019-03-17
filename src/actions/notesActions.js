efb4167069e622f9796a5d14291b0c701bd3106c
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-16 20:06:54 -0700

    redux: note action creators

--- src/actions/notesActions.js
+++ src/actions/notesActions.js
@@ -1,3 +1,5 @@
+import {notes} from "../dummy-data";
+
 export const FETCHING_NOTES = "FETCHING_NOTES";
 export const FETCH_NOTES_SUCCESS = "FETCH_NOTES_SUCCESS";
 export const FETCH_NOTES_FAIL = "FETCH_NOTES_FAIL";
@@ -10,3 +12,19 @@ export const DELETING_NOTE_FAIL = "DELETING_NOTE_FAIL";
 export const UPDATING_NOTE = "UPDATING_NOTE";
 export const UPDATE_NOTE_SUCCESS = "UPDATE_NOTE_SUCCESS";
 export const UPDATE_NOTE_FAIL = "UPDATE_NOTE_FAIL";
+
+export const fetchNotes = () => {
+  return {type: FETCH_NOTES_SUCCESS, payload: notes};
+};
+
+export const createNote = newNote => {
+  return {type: CREATE_NOTE_SUCCESS, payload: newNote};
+};
+
+export const deleteNote = id => {
+  return {type: DELETING_NOTE_SUCCESS, payload: id};
+};
+
+export const updateNote = (id, updatedNote) => {
+  return {type: UPDATE_NOTE_SUCCESS, payload: {id, updatedNote}};
+};
