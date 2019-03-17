e935b79204b729439e12bc7e7787a182b13dcfa9
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-16 19:54:31 -0700

    renders list of cards

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import {MuiThemeProvider} from "@material-ui/core/styles";
 
 import theme from "../styles/theme";
 import NavBar from "./Navigation/NavBar";
+import CardList from "./View/Cards/CardList";
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <NavBar />
+        <CardList />
       </MuiThemeProvider>
     );
   }
