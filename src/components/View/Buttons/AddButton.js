6649873e45ca6f6da23eb3c92c1f6cc59029f4e3
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-17 22:40:42 -0700

    AddButton custom styling

--- src/components/View/Buttons/AddButton.js
+++ src/components/View/Buttons/AddButton.js
@@ -3,12 +3,18 @@ import {withStyles} from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 
-const AddButton = props => {
-  const {classes} = props;
-  console.log(classes);
+const styles = {
+  Button: {
+    position: "absolute",
+    bottom: "30px",
+    right: "30px"
+  }
+};
+
+const AddButton = () => {
   return (
     <div>
-      <Fab color="secondary" aria-label="Add" position="fixed">
+      <Fab color="secondary" aria-label="Add" style={styles.Button}>
         <AddIcon />
       </Fab>
     </div>
