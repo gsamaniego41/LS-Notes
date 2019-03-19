1e0c33c6efc2924de9c9164e497052715ba6ae20
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 22:38:44 -0700

    menu: scaled down topic items

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,15 +4,23 @@ import {
   ListItem,
   ListItemText,
   ListItemIcon,
+  ListItemAvatar,
   Divider,
   Typography,
   Avatar
 } from "@material-ui/core";
 import {Home, Inbox, Settings} from "@material-ui/icons";
 
+const styles = {
+  Avatar: {
+    width: "20px",
+    height: "20px"
+  }
+};
+
 const Menu = props => {
   return (
-    <List style={{marginTop: "100px"}}>
+    <List style={{marginTop: "70px"}}>
       <ListItem button>
         <ListItemIcon>
           <Home />
@@ -36,40 +44,58 @@ const Menu = props => {
       {/* ======= TOPICS ======== */}
       <Typography variant="caption">TOPICS</Typography>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://www.shareicon.net/data/256x256/2016/07/08/117367_logo_512x512.png" />
-        </ListItemIcon>
-        <ListItemText primary="react" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://www.shareicon.net/data/256x256/2016/07/08/117367_logo_512x512.png"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="react" />
       </ListItem>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://cdn.iconscout.com/icon/free/png-256/redux-283024.png" />
-        </ListItemIcon>
-        <ListItemText primary="redux" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://cdn.iconscout.com/icon/free/png-256/redux-283024.png"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="redux" />
       </ListItem>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png" />
-        </ListItemIcon>
-        <ListItemText primary="node" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="node" />
       </ListItem>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://miro.medium.com/max/480/1*O_NwDtfHb1sirw6CAbyoxg.png" />
-        </ListItemIcon>
-        <ListItemText primary="less" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://miro.medium.com/max/480/1*O_NwDtfHb1sirw6CAbyoxg.png"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="less" />
       </ListItem>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://hackr.io/tutorials/javascript/logo-javascript.svg?ver=1550818039" />
-        </ListItemIcon>
-        <ListItemText primary="javascript" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://hackr.io/tutorials/javascript/logo-javascript.svg?ver=1550818039"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="javascript" />
       </ListItem>
       <ListItem button>
-        <ListItemIcon>
-          <Avatar src="https://hackr.io/tutorials/javascript/logo-javascript.svg?ver=1550818039" />
-        </ListItemIcon>
-        <ListItemText primary="es6" />
+        <ListItemAvatar>
+          <Avatar
+            src="https://hackr.io/tutorials/javascript/logo-javascript.svg?ver=1550818039"
+            style={styles.Avatar}
+          />
+        </ListItemAvatar>
+        <ListItemText secondary="es6" />
       </ListItem>
     </List>
   );
